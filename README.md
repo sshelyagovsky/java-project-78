6066432ec41bd0af676507a221b92d77a4166257
s.shelyagovsky <s.shelyagovsky@gmail.com>
2024-05-30 22:52:50 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![Java CI](https://github.com/sshelyagovsky/java-project-78/actions/workflows/main.yaml/badge.svg)](https://github.com/sshelyagovsky/java-project-78/actions/workflows/main.yaml)
+[![Maintainability](https://api.codeclimate.com/v1/badges/59f48ede5cb93c0cb0df/maintainability)](https://codeclimate.com/github/sshelyagovsky/java-project-78/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/59f48ede5cb93c0cb0df/test_coverage)](https://codeclimate.com/github/sshelyagovsky/java-project-78/test_coverage)
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/sshelyagovsky/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/sshelyagovsky/java-project-78/actions)
---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.50.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
